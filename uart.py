49570c0fc6a19127719daf98b4ce9a86c2bf92fb
Kacy Koelliker <kacy@koellikers.com>
2019-06-22 19:23:11 -0700

    change uart.py so it just outputs the current pH reading

--- uart.py
+++ uart.py
@@ -60,62 +60,17 @@ if __name__ == "__main__":
 	
 	real_raw_input = vars(__builtins__).get('raw_input', input) # used to find the correct function for python2/3
 	
-	print("\nWelcome to the Atlas Scientific Raspberry Pi UART example.\n")
-	print("    Any commands entered are passed to the board via UART except:")
-	print("    Poll,xx.x command continuously polls the board every xx.x seconds")
-	print("    Pressing ctrl-c will stop the polling\n")
-	print("    Press enter to receive all data in buffer (for continuous mode) \n")
-
-	# to get a list of ports use the command: 
-	# python -m serial.tools.list_ports
-	# in the terminal
 	usbport = '/dev/ttyAMA0' # change to match your pi's setup 
 
-	print( "Opening serial port now...")
+        try:
+                ser = serial.Serial(usbport, 9600, timeout=0)
+        except serial.SerialException as e:
+                print "Error, ", e
+                sys.exit(0)
 
-	try:
-		ser = serial.Serial(usbport, 9600, timeout=0)
-	except serial.SerialException as e:
-		print( "Error, ", e)
-		sys.exit(0)
+#       send_cmd("RT,26.6667")
+        send_cmd("R")
+        time.sleep(1.3)
+        lines = read_lines()
+        print lines[0]
 
-	while True:
-		input_val = real_raw_input("Enter command: ")
-
-		# continuous polling command automatically polls the board
-		if input_val.upper().startswith("POLL"):
-			delaytime = float(string.split(input_val, ',')[1])
-	
-			send_cmd("C,0") # turn off continuous mode
-			#clear all previous data
-			time.sleep(1)
-			ser.flush()
-			
-			# get the information of the board you're polling
-			print("Polling sensor every %0.2f seconds, press ctrl-c to stop polling" % delaytime)
-	
-			try:
-				while True:
-					send_cmd("R")
-					lines = read_lines()
-					for i in range(len(lines)):
-						# print lines[i]
-						if lines[i][0] != '*':
-							print( "Response: " , lines[i])
-					time.sleep(delaytime)
-
-			except KeyboardInterrupt: 		# catches the ctrl-c command, which breaks the loop above
-				print("Continuous polling stopped")
-	
-		# if not a special keyword, pass commands straight to board
-		else:
-			if len(input_val) == 0:
-				lines = read_lines()
-				for i in range(len(lines)):
-					print( lines[i])
-			else:
-				send_cmd(input_val)
-				time.sleep(1.3)
-				lines = read_lines()
-				for i in range(len(lines)):
-					print( lines[i])
